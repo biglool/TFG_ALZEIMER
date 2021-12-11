289825dc19a0bbdd3a312f399273130ca742b52a
biglool <biglool@gmail.com>
2021-12-11 22:14:40 +0100

    Update models.py

--- code/models.py
+++ code/models.py
@@ -1,6 +1,6 @@
 
-import tfg.code.models.densenet3D as DN3
-import tfg.code.models.resnet3D as Res3
+from tfg.code.models import densenet3D as DN3
+from tfg.code.models import resnet3D as Res3
 
 def generate_model(model_depth, **kwargs):
     assert model_depth in ['Densenet121', 'Densenet169', 'Densenet201', 'Densenet264','Resnet10', 'Resnet18', 'Resnet34', 'Resnet50', 'Resnet101', 'Resnet152', 'Resnet200']
