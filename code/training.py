7c28505124cfdc8f78f47cdd474056e90364f4d5
biglool <biglool@gmail.com>
2021-12-11 22:31:39 +0100

    Update training.py

--- code/training.py
+++ code/training.py
@@ -31,7 +31,7 @@ def simpleTrainExperiment(carpeta, nom, device, dataset, problemType, cut, model
 		print(train_idx)
 		print(valid_idx)
 		print(test_idx)
-		databaseinfo=databaseInfo(problemType,cut,train_idx,valid_idx,test_idx)
+		databaseinfo=databaseInfo(problemType,cut,1,2,3)
 		config=sessionConfig(model,lr,batch_size,paciencia)		
 		sessioninfo= trainSessionInfo()
 
