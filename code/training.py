be7fff18c85c0ad5651c377ae035b477fc4d05a1
biglool <biglool@gmail.com>
2021-12-11 22:29:12 +0100

    Update training.py

--- code/training.py
+++ code/training.py
@@ -26,7 +26,11 @@ def simpleTrainExperiment(carpeta, nom, device, dataset, problemType, cut, model
 	    	#fem split si no s'ha fet (70/15/15)
 		train_idx, valid_idx, train_targs, valid_targs = train_test_split(np.arange(dataset.__len__()),dataset.targets(),test_size=0.3,shuffle=True, stratify=dataset.targets())
 		valid_idx, test_idx= train_test_split(valid_idx,test_size=0.5,shuffle=True, stratify=valid_targs)
-
+		print(problemType)
+		print(cut)
+		print(train_idx)
+		print(valid_idx)
+		print(test_idx)
 		databaseinfo=databaseInfo(problemType,cut,train_idx,valid_idx,test_idx)
 		config=sessionConfig(model,lr,batch_size,paciencia)		
 		sessioninfo= trainSessionInfo()
