074afb8568d4f11e056615313045ced985f22532
biglool <biglool@gmail.com>
2021-12-11 22:00:28 +0100

    Update training.py

--- code/training.py
+++ code/training.py
@@ -9,77 +9,77 @@ from sklearn.metrics import confusion_matrix,accuracy_score,f1_score
 
 def simpleTrainExperiment(carpeta, nom, device, dataset, problemType, cut, model, batch_size, lr, paciencia, verbose=False):
 
-# carreguem dades o inicialitzem si primer run
-if os.path.isfile(carpeta + nom ):
-	databaseinfo,config,sessioninfo,modelstate= loadCheck(carpeta, nom)
+	# carreguem dades o inicialitzem si primer run
+	if os.path.isfile(carpeta + nom ):
+		databaseinfo,config,sessioninfo,modelstate= loadCheck(carpeta, nom)
 
-	dataset.setProblem(databaseinfo.problemType)
-	dataset.setCut(databaseinfo.cutId)
+		dataset.setProblem(databaseinfo.problemType)
+		dataset.setCut(databaseinfo.cutId)
 
-else:
-	
-	dataset.setProblem(problemType)
-	dataset.setCut(cut)	
-	
-    #fem split si no s'ha fet (70/15/15)
-	train_idx, valid_idx, train_targs, valid_targs = train_test_split(np.arange(dataset.__len__()),dataset.targets(),test_size=0.3,shuffle=True, stratify=dataset.targets())
-	valid_idx, test_idx= train_test_split(valid_idx,test_size=0.5,shuffle=True, stratify=valid_targs)
+	else:
 
-	databaseinfo=databaseInfo(problemType,cut,train_idx,valid_idx,test_idx)
-	config=sessionConfig(model,lr,batch_size,paciencia)		
-	sessioninfo= trainSessionInfo()
+		dataset.setProblem(problemType)
+		dataset.setCut(cut)	
 
-# carguem objectes 
-num_epochs = 200
-batch_size=config.batch_size
+	    	#fem split si no s'ha fet (70/15/15)
+		train_idx, valid_idx, train_targs, valid_targs = train_test_split(np.arange(dataset.__len__()),dataset.targets(),test_size=0.3,shuffle=True, stratify=dataset.targets())
+		valid_idx, test_idx= train_test_split(valid_idx,test_size=0.5,shuffle=True, stratify=valid_targs)
 
-model=generate_model(config.modelId)
-model.to(device)	
-optimizer = optim.Adam(model.parameters(), lr = config.lr) 	
-earlyStoper=ut.EarlyStoper(config.paciencia)
-loss_func = nn.CrossEntropyLoss() 
+		databaseinfo=databaseInfo(problemType,cut,train_idx,valid_idx,test_idx)
+		config=sessionConfig(model,lr,batch_size,paciencia)		
+		sessioninfo= trainSessionInfo()
 
-loaders = getLoaders(databaseinfo.train_idx,databaseinfo.valid_idx,databaseinfo.test_idx)
+	# carguem objectes 
+	num_epochs = 200
+	batch_size=config.batch_size
 
-if os.path.isfile(carpeta + nom ):
-	model.load_state_dict(modelState.statedic)
-	optimizer.load_state_dict(modelState.optdic)
-	
-# run
+	model=generate_model(config.modelId)
+	model.to(device)	
+	optimizer = optim.Adam(model.parameters(), lr = config.lr) 	
+	earlyStoper=ut.EarlyStoper(config.paciencia)
+	loss_func = nn.CrossEntropyLoss() 
 
-for epoch in range(num_epochs):
-	config.epoch= epoch+config.epoch
-	train_loss,train_y_true, train_y_pred=train(model, loaders, optimizer,loss_func,batch_size, device)   
-	valid_loss ,valid_y_true, valid_y_pred=validate(model, loaders, optimizer,loss_func,batch_size, device)
+	loaders = getLoaders(databaseinfo.train_idx,databaseinfo.valid_idx,databaseinfo.test_idx)
+
+	if os.path.isfile(carpeta + nom ):
+		model.load_state_dict(modelState.statedic)
+		optimizer.load_state_dict(modelState.optdic)
+
+	# run
+
+	for epoch in range(num_epochs):
+		config.epoch= epoch+config.epoch
+		train_loss,train_y_true, train_y_pred=train(model, loaders, optimizer,loss_func,batch_size, device)   
+		valid_loss ,valid_y_true, valid_y_pred=validate(model, loaders, optimizer,loss_func,batch_size, device)
 
 
-	train_cfm = confusion_matrix(train_y_true, train_y_pred)
-	train_accu = accuracy_score(train_y_true, train_y_pred)
+		train_cfm = confusion_matrix(train_y_true, train_y_pred)
+		train_accu = accuracy_score(train_y_true, train_y_pred)
 
-	valid_cfm = confusion_matrix(valid_y_true, valid_y_pred)
-	valid_accu = accuracy_score(valid_y_true, valid_y_pred)
-	f1=f1_score(valid_y_true, valid_y_pred)
+		valid_cfm = confusion_matrix(valid_y_true, valid_y_pred)
+		valid_accu = accuracy_score(valid_y_true, valid_y_pred)
+		f1=f1_score(valid_y_true, valid_y_pred)
 
-	config.train_accus.append(train_accu)
-	config.train_losses.append(train_loss)
-	connfig.valid_losses.append(valid_loss)
-	config.valid_accus.append(valid_accu)
-	config.train_confs.append(train_cfm)
-	config.valid_confs.append(valid_cfm)
-	
-	if verbose:
-        	ut.printEpochResult(config.epoch,train_accu,train_loss,valid_accu,valid_loss,train_cfm,valid_cfm)
-    
-	stopSave = earlyStoper.update(f1)
+		config.train_accus.append(train_accu)
+		config.train_losses.append(train_loss)
+		connfig.valid_losses.append(valid_loss)
+		config.valid_accus.append(valid_accu)
+		config.train_confs.append(train_cfm)
+		config.valid_confs.append(valid_cfm)
 
-	if stopSave==True:
-		modelstate= modelState(model.state_dict(),optimizer.state_dict())
-		saveCheck(carpeta,nom,databaseinfo,config,modelstate,sessioninfo)   
-		
 		if verbose:
-			printConf(train_cfm)
-			printConf(valid_cfm)
-			printGrafs( config.train_accus, config.train_losses, config.valid_accus, config.valid_losses)		
+			ut.printEpochResult(config.epoch,train_accu,train_loss,valid_accu,valid_loss,train_cfm,valid_cfm)
 
-	if earlyStoper.paciencia==0:
-		break
+		stopSave = earlyStoper.update(f1)
+
+		if stopSave==True:
+			modelstate= modelState(model.state_dict(),optimizer.state_dict())
+			saveCheck(carpeta,nom,databaseinfo,config,modelstate,sessioninfo)   
+
+			if verbose:
+				printConf(train_cfm)
+				printConf(valid_cfm)
+				printGrafs( config.train_accus, config.train_losses, config.valid_accus, config.valid_losses)		
+
+		if earlyStoper.paciencia==0:
+			break
