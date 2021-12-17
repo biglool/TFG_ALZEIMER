0c46904762d856161bfe906db7fa7e76bac38bd6
biglool <biglool@gmail.com>
2021-12-17 20:04:24 +0100

    Update ensamble.py

--- code/ensamble.py
+++ code/ensamble.py
@@ -81,11 +81,16 @@ def crossValidateVoteMax(models, device, dataset, K=5,verbose=False):
 	resultats=[]
 	for fold in range(1,5):
 		models_fold= [[model, "fold"+ str(fold) +".pt"] for model in models]
-		true, pred =voteMax(models, device, dataset, verbose=verbose)
+		true, pred =voteMax(models, device, dataset)
 		metrics= ut.getMetrics(0,y_true, y_pred)
 		resultats.append(resultat)
 		if verbose:
-			
+			print("Resultat Fold:"+ str(fold) )	
+			ut.printMetrics(metrics)		
+	res_final= [sum(met)/len(met) for met in np.array(resultats).T.tolist()]
+	if verbose:
+		print("Resultat Final:" )	
+		ut.printMetrics(res_final)
 		
-	return [sum(met)/len(met) for met in np.array(resultats).T.tolist()]
+	return res_final
 		
