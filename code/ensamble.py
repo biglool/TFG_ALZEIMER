0afa716bf133ac7a4ae2a2a1ba50620e82267c49
biglool <biglool@gmail.com>
2021-12-17 17:45:22 +0100

    Update ensamble.py

--- code/ensamble.py
+++ code/ensamble.py
@@ -27,8 +27,8 @@ def getPred(model, loaders, device,outType="preds",val_type='test'):
 			elif outType=="probs":
 				sm = torch.nn.Softmax(dim=1)
 				probabilities = sm(outputs) 
-				print(probabilities)
-				y_pred.extend(probabilities[0].cpu().numpy()) 
+				print(probabilities.data.cpu().numpy())
+				y_pred.extend(probabilities.data.cpu().numpy()) 
 
 			labels = labels.data.cpu().numpy()
 			y_true.extend(labels) 
