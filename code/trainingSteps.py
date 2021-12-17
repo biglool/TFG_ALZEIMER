316a5cfb41c3c9decf854c882baaffe53e31099b
biglool <biglool@gmail.com>
2021-12-17 16:10:52 +0100

    Update trainingSteps.py

--- code/trainingSteps.py
+++ code/trainingSteps.py
@@ -55,7 +55,9 @@ def validate(model, loaders,optimizer,loss_func,batch_size, device,val_type='val
 
 			outputs=model(images)
 			loss=loss_func(outputs,labels)
-			print(outputs)
+			 sm = torch.nn.Softmax(dim=1)
+            		probabilities = sm(outputs)	
+			print(probabilities)
 			# info
 			preds = (torch.max(torch.exp(outputs), 1)[1]).data.cpu().numpy()
 			y_pred.extend(preds) 
